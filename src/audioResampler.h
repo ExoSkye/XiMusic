81e8b05dcfea8d93de4f455ab9d74b52c8119f03
ProtoByter <bobrow2006@gmail.com>
2021-09-28 18:04:08 +0100

    Remove accidental addition of wasapi player from the main sources list

--- src/audioResampler.h
+++ src/audioResampler.h
@@ -1,10 +1,25 @@
+/** @file */
 #pragma once
 
 #include <stdbool.h>
 #include <audioPlayer.h>
 
-void resampler_init();
+/**
+ * Initialize the resampler.
+ */
+void resampler_init(int channels);
 
+
+/**
+ * Run the resampler on a chunk.
+ * @param chunk the chunk to resample.
+ * @param current_freq The current frequency of that chunk.
+ * @param wanted_freq The frequency that is required.
+ * @return Whether the resampler was successful. (ie no errors)
+ */
 bool resampleChunk(AudioChunk* chunk, int current_freq, int wanted_freq);
 
+/**
+ * Free the resampler.
+ */
 void resampler_free();
